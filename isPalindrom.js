ee6bc01db5622571950dbed0cf800edbaedd289c
Generosa Litton <glcodeworks@gmail.com>
2024-05-08 16:37:49 -0500

    added function that is case insensitive, only takes in to account alphanumric

--- isPalindrom.js
+++ isPalindrom.js
@@ -7,6 +7,15 @@ function isPalindrome(string) {
   return string === reversedString;
 }
 
-console.log(isPalindrome("Madam"));
-console.log(isPalindrome("madam i'm adam"));
-console.log(isPalindrome("356653"));
+// console.log(isPalindrome("Madam"));
+// console.log(isPalindrome("madam i'm adam"));
+// console.log(isPalindrome("356653"));
+
+function isRealPalindrome(string) {
+  string = string.toLowerCase().replace(/[^a-z0-9]/g, ""); //remove spaces and special chars
+  const reversedCleanString = string.split("").reverse().join(""); // reverse and join
+  return string.toLowerCase() === reversedCleanString; //compare
+}
+
+console.log(isRealPalindrome("Madam"));
+console.log(isRealPalindrome("Madam, I'm Adam"));
