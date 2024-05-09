6e7f05b9884fcef1198a1f8529ce11119803dba1
Generosa Litton <glcodeworks@gmail.com>
2024-05-09 18:26:50 -0500

    hex to Int done

--- stringToInteger.js
+++ stringToInteger.js
@@ -16,8 +16,43 @@ function stringToInteger(string) {
 
   let value = 0;
 
-  arrayOfNumbers.forEach((num) => (value = num + 10 * value));
+  arrayOfNumbers.forEach((num) => (value = num + BASE * value));
   return value;
 }
 
-console.log(stringToInteger("4321") === 4321);
+// console.log(stringToInteger("4321") === 4321);
+
+function hexadecimalToInteger(string) {
+  const HEX_BASE = 16;
+  const HEX_DIGITS = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    A: 10,
+    B: 11,
+    C: 12,
+    D: 13,
+    E: 14,
+    F: 15,
+  };
+
+  let arrayOfCharacters = string
+    .toUpperCase()
+    .split("")
+    .map((char) => HEX_DIGITS[char]);
+
+  console.log(arrayOfCharacters);
+  let hexValue = 0;
+
+  arrayOfCharacters.forEach((char) => (hexValue = char + HEX_BASE * hexValue));
+  return hexValue;
+}
+
+console.log(hexadecimalToInteger("4D9f"));
