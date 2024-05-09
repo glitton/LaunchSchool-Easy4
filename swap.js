3a0202d1af63e7b9ea8ac12971f7f92e791ce165
Generosa Litton <glcodeworks@gmail.com>
2024-05-09 15:31:28 -0500

    swap analysis done

--- swap.js
+++ swap.js
@@ -1,7 +1,36 @@
-function swap(string) {
-  const reversedString = string.split("").reverse().join("");
+function swapChars(string) {
+  const reversedString = string
+    .split("")
+    .reverse()
+    .join("")
+    .split(" ")
+    .reverse()
+    .join(" ");
+
   return reversedString;
 }
 
-console.log(swap("Osay"));
-console.log(swap("Oh what a wonderful day it is"));
+// console.log(swap("Osay"));
+// console.log(swap("Oh what a wonderful day it is"));
+
+// Launch School solution
+function swap(words) {
+  let wordsArray = words.split(" ");
+
+  for (let idx = 0; idx < wordsArray.length; idx += 1) {
+    wordsArray[idx] = swapFirstLastCharacters(wordsArray[idx]);
+  }
+
+  return wordsArray.join(" ");
+}
+
+function swapFirstLastCharacters(word) {
+  if (word.length === 1) {
+    return word;
+  }
+
+  return word[word.length - 1] + word.slice(1, -1) + word[0];
+}
+
+// console.log(swapFirstLastCharacters("what"));
+// console.log(swap("Oh what a wonderful day it is"));
